11fc35699d4d45f3e96a773b2bfc96e143e4f8c2
Alondra Martinez <alondra4643@gmail.com>
2023-03-23 21:34:23 -0400

    First Draft

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HangmanAnimalEdition
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Welcome to Hangman!- Animal Edition");
+			Console.WriteLine("------------------------\n");
+			Console.WriteLine("Enter your first guess");
+
+			string[] listOfWordsAvailable = new string[5];
+
+			listOfWordsAvailable[0] = "horse";
+			listOfWordsAvailable[1] = "koala";
+			listOfWordsAvailable[2] = "moose";
+			listOfWordsAvailable[3] = "camel";
+			listOfWordsAvailable[4] = "mouse";
+
+			Class1 class1 = new Class1();
+		}
+	}
+}
