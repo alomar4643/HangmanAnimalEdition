76b045925db8aefb199251ab172e83a1801356d0
Alondra Martinez <alondra4643@gmail.com>
2023-03-25 20:13:30 -0400

    Second Commit

--- Class1.cs
+++ Class1.cs
@@ -8,6 +8,18 @@ namespace HangmanAnimalEdition
 {
 	public class Class1
 	{
+		public List<int> findIndexes(string chosenWord, char guessLetter)
+		{
+			List<int> indexes = new List<int>();
 
+			for (int h = 0; h < chosenWord.Length; h++)
+			{
+				if (guessLetter == chosenWord[h])
+				{
+					indexes.Add(h);
+				}
+			}
+			return indexes;
+		}
 	}
 }
--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ namespace HangmanAnimalEdition
 	{
 		static void Main(string[] args)
 		{
+			Console.WriteLine("Press ~ to exit\n");
 			Console.WriteLine("Welcome to Hangman!- Animal Edition");
 			Console.WriteLine("------------------------\n");
 			Console.WriteLine("Enter your first guess");
@@ -23,6 +24,66 @@ namespace HangmanAnimalEdition
 			listOfWordsAvailable[4] = "mouse";
 
 			Class1 class1 = new Class1();
+
+			//Random object will generate a random word from the list above
+			Random random = new Random();
+			var index = random.Next(listOfWordsAvailable.Length);
+			var lives = 6;
+			string chosenWord = listOfWordsAvailable[index];
+			char[] guess = new char[chosenWord.Length];
+
+			//i++ increments the variable by 1
+			for (int i = 0; i < chosenWord.Length; i++)
+
+				//char guess displays the number of letters in the words of the list of words available. This is represented by the letter 'X'
+				guess[i] = '*';
+
+			while (true)
+			{
+				try
+				{
+					//converts value of string into a char
+					char playersGuess = char.Parse(Console.ReadLine());
+
+					if (chosenWord.ToLower().IndexOf(playersGuess) != -1)
+					{
+						Console.WriteLine("Correct guess, Keep going!");
+
+						List<int> ints = class1.findIndexes(chosenWord.ToLower(), playersGuess);
+
+						for (int j = 0; j < ints.Count; j++)
+						{
+							guess[ints[j]] = playersGuess;
+						}
+						if (Array.IndexOf(guess, '*') == -1)
+						{
+							Console.WriteLine("Congrats you won!");
+							break;
+						}
+					}
+					else
+					{
+						lives--;//lives = lives minus 1
+						Console.WriteLine("Incorrect guess, try another! " + "You have: " + lives + " lives left");
+
+						if (lives == 0)
+						{
+							Console.WriteLine("Sorry you Lost!");
+							break;
+						}
+					}
+					if (playersGuess == '~')
+					{
+						break;
+					}
+				}
+				catch (FormatException)
+				{
+					Console.WriteLine("Please enter a single letter guess");
+				}
+				Console.WriteLine(guess);
+			}
 		}
 	}
 }
+
